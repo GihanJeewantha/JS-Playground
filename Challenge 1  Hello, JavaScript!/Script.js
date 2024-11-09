84fb314477379f2b8938a2516acacb19d7fc0bef
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2024-11-09 19:02:13 +0530

    Challenge #1: Hello, JavaScript!

---
+++ Challenge 1  Hello, JavaScript!/Script.js
@@ -0,0 +1,3 @@
+// Write a program that outputs the phrase "Hello, JavaScript!" to the console.
+
+console.log("Hello Wrold !");
--- Link JS to HTML/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello World !");
-
-let myVariable;
-
-myVariable = 25;
-
-console.log(typeof myVariable);
-
-console.log("This is first try!".length)
